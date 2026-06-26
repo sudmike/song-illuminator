cca69627ba4edb900bf148d27a09a23b10c1e6c7
sudmike <65828552+sudmike@users.noreply.github.com>
2026-06-26 13:46:08 +0200

    Runners: artifact registry

---
+++ runners/iac/artifact-registry/outputs.tf
@@ -0,0 +1,13 @@
+output "artifact_registry_repository_uri" {
+  description = "The artifact registry repository uri."
+  value       = google_artifact_registry_repository.github_runners.registry_uri
+}
+
+output "github_artifact_registry_repository_uri_variable_command" {
+  description = "Run this command to store the Terraform artifact registry repository uri as a GitHub environment variable."
+  value       = <<EOT
+gh variable set ARTIFACT_REGISTRY_REPOSITORY_URI \
+  --env <environment> \
+  --body "${google_artifact_registry_repository.github_runners.registry_uri}"
+EOT
+}
