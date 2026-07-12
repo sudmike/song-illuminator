fc5c065b28c262f76f632a9e261da06e63bb2234
sudmike <65828552+sudmike@users.noreply.github.com>
2026-07-12 22:33:01 +0200

    App: Fix image uri

--- app/iac/compute/main.tf
+++ app/iac/compute/main.tf
@@ -52,7 +52,7 @@ resource "google_cloud_run_v2_service" "app" {
     service_account = google_service_account.app.email
 
     containers {
-      image = var.image_uri
+      image = "${var.image_uri}/app:latest"
 
       resources {
         cpu_idle = false
